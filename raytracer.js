0f5bcddc8e9bb21955fa4a39fc4fc23994219ad0
Emiliano1919 <emilianojg38@gmail.com>
2025-03-25 21:06:21 -0400

    Add vector, scene, sphere, canvasToViewport

--- raytracer.js
+++ raytracer.js
@@ -28,13 +28,68 @@ class Color{
     }
 
     add(other) {
-
         return new Color(
           this.#evaluateAddition(this.r,other.r),
           this.#evaluateAddition(this.g,other.g),
           this.#evaluateAddition(this.b,other.b)
         );
     }
+}
+class Sphere{
+    constructor(centre, radius, color){
+        this.centre=centre;
+        this.radius=radius;
+        this.color=color;
+    }
+}
+
+class Vector {
+    constructor(x, y, z = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    add(v) {
+        return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+    }
+
+    subtract(v) {
+        return new Vector(this.x - v.x, this.y - v.y, this.z - v.z);
+    }
+
+    dot(v) {
+        return this.x * v.x + this.y * v.y + this.z * v.z;
+    }
+
+    cross(v) {
+        return new Vector(
+            this.y * v.z - this.z * v.y,
+            this.z * v.x - this.x * v.z,
+            this.x * v.y - this.y * v.x
+        );
+    }
+
+    magnitude() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
+    }
+
+    normalize() {
+        let mag = this.magnitude();
+        return mag === 0 ? new Vector(0, 0, 0) : new Vector(this.x / mag, this.y / mag, this.z / mag);
+    }
+
+    scale(scalar) {
+        return new Vector(this.x * scalar, this.y * scalar, this.z * scalar);
+    }
+
+    toString() {
+        return `(${this.x}, ${this.y}, ${this.z})`;
+    }
+}
+
+function canvasToViewport(x,y) {
+    return new Vector(x*(viewport_width/canvas.width),y*(viewport_height/canvas.height),1)
 }
 
 function putPixel(x, y, color) {
@@ -48,5 +103,17 @@ function putPixel(x, y, color) {
     data[index + 1] = color.g; 
     data[index + 2] = color.b;
     data[index + 3] = 255;
-    
 }
+
+// The Scene
+const viewport_width = 1;
+const viewport_height =1;
+const projection_plane_z = 1;
+const camera_position = new Vector(0, 0, 0);
+const background_color = new Color(255, 255, 255);
+const spheres = [
+  new Sphere(new Vector(0, -1, 3), 1, new Color(255, 0, 0)),
+  new Sphere(new Vector(-2, 0, 4), 1, new Color(0, 255, 0)),
+  new Sphere(new Vector(2, 0, 4), 1, new Color(0, 0, 255)),
+  new Sphere(new Vector(0, -5001, 0), 5000, new Color(255, 255, 0))
+];
